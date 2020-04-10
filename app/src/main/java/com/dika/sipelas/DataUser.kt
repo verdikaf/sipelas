b63ed56184e507929ef4fbf2b4c7146140b23fa4
dika fajar <verdikaf@gmail.com>
2020-04-10 20:12:25 +0700

    Initial commit

---
+++ app/src/main/java/com/dika/sipelas/DataUser.kt
@@ -0,0 +1,12 @@
+package com.dika.sipelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataUser : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_user)
+    }
+}
---
+++ app/src/main/java/com/dika/sipelas/dataPeminjaman.kt
@@ -0,0 +1,12 @@
+package com.dika.sipelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dataPeminjaman : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_peminjaman)
+    }
+}
